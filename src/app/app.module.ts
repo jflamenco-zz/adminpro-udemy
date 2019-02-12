b162db7ad7641246891f464ac6775cd9ee950234
Julio Flamenco <jflamenco@outlook.com>
2019-02-11 18:54:45 -0600

    Prueba

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { PagesModule } from './pages/pages.module';
+
+// Rutas
+import { APP_ROUTES } from './app.routes';
+
+// Componentes
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    PagesModule,
+    FormsModule,
+    APP_ROUTES
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
